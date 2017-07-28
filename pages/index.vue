7c27694ca439a6209caf2ff57f2aa377be9c3706
flyc <flychung@fashionguide.com.tw>
2017-07-28 15:18:17 +0800

    add script tag of pages
    verys simple one

--- pages/index.vue
+++ pages/index.vue
@@ -1,3 +1,18 @@
 <template>
-	<p>Hello</p>
+	<p>{{msg}}</p>
 </template>
+
+<script>
+    export default {
+        asyncData: function() {
+            return {
+                msg: 'hello'
+            };
+        },
+        data: function() {
+            return{
+
+            };
+        }
+    };
+</script>
