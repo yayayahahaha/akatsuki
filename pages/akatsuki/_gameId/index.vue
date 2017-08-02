5d74f754bd73b397338776687bda4a1f64162161
FlyC <yayayahahahaooii@gmail.com>
2017-08-02 14:17:03 +0800

    a little bit weried, commit it first and push to cloud9 for testing

---
+++ pages/akatsuki/_gameId/index.vue
@@ -0,0 +1,35 @@
+<template>
+	<div>
+		<p>{{msg}}</p>
+	</div>
+</template>
+
+<style></style>
+
+<script>
+	export default {
+	    asyncData: function(context) {
+	        return {
+	            msg: context.params
+	        };
+	    },
+	    data: function() {
+	        return {
+
+	        };
+	    },
+	    head: function(context) {
+	    	if (context) {
+	    		console.log(context.msg);
+	    		return{
+		    		title: context.msg.gameId
+		    	};
+	    	}else{
+	    		console.log("nothing");
+		    	return {
+		    		title: 'basic title'
+		    	}
+	    	}
+	    }
+	}
+</script>
--- pages/akatsuki/index.vue
+++ pages/akatsuki/index.vue
@@ -8,15 +8,36 @@
 
 <script>
 	export default {
-		asyncData: function( context ) {
-			return {
-				msg: 'hello'
+		asyncData: function(context) {
+
+			var array = [];
+			if (context) {
+				for(var key in context){
+					console.log(key);
+				}
 			}
+			return {
+				params: context.params
+			};
 		},
 		data: function() {
 			return {
-
+				msg:'hello'
 			}
+		},
+		head: function(context) {
+
+			if (context) {
+				console.log(context.params);
+			}
+
+			return {
+				link:[{
+					rel:"stylesheet" ,
+					type:"text/css" ,
+					href:"/css/akatsuki.css"
+				}]
+			};
 		}
 	}
 </script>
