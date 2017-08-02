6be1fcf2965714e487351db1be8212fc7c6b17e2
FlyC <yayayahahahaooii@gmail.com>
2017-08-02 13:45:20 +0800

    create akatsuki-resume route, only with simple example

---
+++ pages/akatsuki/index.vue
@@ -0,0 +1,22 @@
+<template>
+	<div>
+		<p v-text="msg"></p>
+	</div>
+</template>
+
+<style></style>
+
+<script>
+	export default {
+		asyncData: function( context ) {
+			return {
+				msg: 'hello'
+			}
+		},
+		data: function() {
+			return {
+
+			}
+		}
+	}
+</script>
