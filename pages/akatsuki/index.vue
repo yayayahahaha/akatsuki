7511cc47ece082f58f292f3248f0a5bb740098b4
FlyC <yayayahahahaooii@gmail.com>
2017-08-02 17:37:21 +0800

    words background works

--- pages/akatsuki/index.vue
+++ pages/akatsuki/index.vue
@@ -1,5 +1,10 @@
 <template>
 	<div>
+		<div class="background-class">
+			<template v-for='time in backTimes'>
+				<span class="back_word" v-for='item in backWord' v-text='item'></span>
+			</template>
+		</div>
 		<p v-text="msg"></p>
 		<p>useless css code like top without position in hire page (https://aktsk.com.tw/tw_recruit/interview/day01_engineer.html)</p>
 		<p>url with .html, which means without router or any front-end framework?</p>
@@ -19,8 +24,42 @@
 
 		},
 		data: function() {
+			function messArray(array) {
+			  var currentIndex = array.length, temporaryValue, randomIndex;
+			  while (0 !== currentIndex) {
+			    randomIndex = Math.floor(Math.random() * currentIndex);
+			    currentIndex -= 1;
+			    temporaryValue = array[currentIndex];
+			    array[currentIndex] = array[randomIndex];
+			    array[randomIndex] = temporaryValue;
+			  }
+			  return array;
+			}
+			var char = 'qazwsxedcrfvtgbyhnujmikolpQAZWSXEDCRFVTGBYHNUJMIKOLP0123456789',
+				tempArray = [], bigArray = [];
+
+				for (var i = 0; i < char.length; i++) {
+					tempArray[i] = char[i];
+				}
+
+				for (var i = 0; i < 30; i++) {
+					tempArray = messArray(tempArray);
+					bigArray = bigArray.concat(tempArray);
+				}
+
 			return {
-				msg:'hello'
+				msg:'hello',
+				backWord: bigArray,
+				backTimes: 1
+			}
+		},
+		mounted: function() {
+			window.word = document.querySelector(".background-class");
+			window.h = window.innerHeight;
+			console.log(h, word.offsetHeight);
+			if (h > word.offsetHeight) {
+				this.backTimes = Math.ceil(h/word.offsetHeight);
+				console.log(this.backTimes);
 			}
 		},
 		head: function(context) {
