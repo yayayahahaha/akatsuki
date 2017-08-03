5a8eab7e6857343879837893d56360e8eaa15310
FlyC <yayayahahahaooii@gmail.com>
2017-08-03 15:39:29 +0800

    self intro page works

--- pages/akatsuki/index.vue
+++ pages/akatsuki/index.vue
@@ -111,9 +111,12 @@
 			            </div>
 			        </div>
 
+			        <div class="view view3">
+			        	<img src="/images/profile2.jpg" alt="profile">
+			        </div>
+
 			        <div class="view view4">
-			            <div class="draw">
-			            </div>
+			        view4
 			        </div>
 			        <div class="view view5">view5 </div>
 			        <div class="view view6">view6 </div>
@@ -213,9 +216,12 @@
 			            </div>
 			        </div>
 
+			        <div class="view view3">
+			        	<img src="/images/timeline.png" alt="timeline">
+			        </div>
+
 			        <div class="view view4">
-			            <div class="draw">
-			            </div>
+			        view4
 			        </div>
 			        <div class="view view5">view5 </div>
 			        <div class="view view6">view6 </div>
@@ -314,9 +320,69 @@
 			            </div>
 			        </div>
 
+			        <div class="view view3">
+			        	<div class="content">
+			        		<span class="show_color">人</span>
+			        		<span class="show_color">稱</span>
+			        		<span class="hide_color name_style">飛</span>
+			        		<span class="show_color name_style">膝</span>
+			        		<br>
+			        		<span class="show_color">個</span>
+			        		<span class="hide_color">性</span>
+			        		<span class="show_color">隨</span>
+			        		<span class="show_color">和</span>
+			        		<span class="show_color">、</span>
+			        		<span class="hide_color">帶</span>
+			        		<span class="show_color">點</span>
+			        		<span class="show_color">幽</span>
+			        		<span class="hide_color">默</span>
+			        		<br>
+			        		<span class="show_color">喜</span>
+			        		<span class="hide_color">愛</span>
+			        		<span class="show_color">動</span>
+			        		<span class="show_color">漫</span>
+			        		<span class="hide_color">與</span>
+			        		<span class="show_color">音</span>
+			        		<span class="show_color">樂</span>
+			        		<br>
+			        		<span class="show_color">期</span>
+			        		<span class="show_color">許</span>
+			        		<span class="hide_color">自</span>
+			        		<span class="show_color">己</span>
+			        		<span class="show_color">可</span>
+			        		<span class="hide_color">以</span>
+			        		<span class="show_color">成</span>
+			        		<span class="show_color">為</span>
+			        		<span class="show_color">被</span>
+			        		<span class="hide_color">別</span>
+			        		<span class="show_color">人</span>
+			        		<span class="show_color">信</span>
+			        		<span class="show_color">任</span>
+			        		<span class="hide_color">的</span>
+			        		<span class="show_color">對</span>
+			        		<span class="show_color">象</span>
+			        		<br>
+			        		<span class="show_color">期</span>
+			        		<span class="show_color">許</span>
+			        		<span class="hide_color">自</span>
+			        		<span class="show_color">己</span>
+			        		<span class="hide_color">可</span>
+			        		<span class="show_color">以</span>
+			        		<span class="show_color">一</span>
+			        		<span class="hide_color">次</span>
+			        		<span class="show_color">又</span>
+			        		<span class="show_color">一</span>
+			        		<span class="show_color">次</span>
+			        		<span class="hide_color">地</span>
+			        		<span class="show_color">感</span>
+			        		<span class="hide_color">動</span>
+			        		<span class="show_color">他</span>
+			        		<span class="show_color">人</span>
+			        	</div>
+			        </div>
+
 			        <div class="view view4">
-			            <div class="draw">
-			            </div>
+			        view4
 			        </div>
 			        <div class="view view5">view5 </div>
 			        <div class="view view6">view6 </div>
@@ -415,9 +481,70 @@
 			            </div>
 			        </div>
 
+			        <div class="view view3">
+			        	<div class="content">
+			        		
+			        		<span class="hide_color">人</span>
+			        		<span class="hide_color">稱</span>
+			        		<span class="show_color name_style">飛</span>
+			        		<span class="hide_color name_style">膝</span>
+			        		<br>
+			        		<span class="hide_color">個</span>
+			        		<span class="show_color">性</span>
+			        		<span class="hide_color">隨</span>
+			        		<span class="hide_color">和</span>
+			        		<span class="hide_color">、</span>
+			        		<span class="show_color">帶</span>
+			        		<span class="hide_color">點</span>
+			        		<span class="hide_color">幽</span>
+			        		<span class="show_color">默</span>
+			        		<br>
+			        		<span class="hide_color">喜</span>
+			        		<span class="show_color">愛</span>
+			        		<span class="hide_color">動</span>
+			        		<span class="hide_color">漫</span>
+			        		<span class="show_color">與</span>
+			        		<span class="hide_color">音</span>
+			        		<span class="hide_color">樂</span>
+			        		<br>
+			        		<span class="hide_color">期</span>
+			        		<span class="hide_color">許</span>
+			        		<span class="show_color">自</span>
+			        		<span class="hide_color">己</span>
+			        		<span class="hide_color">可</span>
+			        		<span class="show_color">以</span>
+			        		<span class="hide_color">成</span>
+			        		<span class="hide_color">為</span>
+			        		<span class="hide_color">被</span>
+			        		<span class="show_color">別</span>
+			        		<span class="hide_color">人</span>
+			        		<span class="hide_color">信</span>
+			        		<span class="hide_color">任</span>
+			        		<span class="show_color">的</span>
+			        		<span class="hide_color">對</span>
+			        		<span class="hide_color">象</span>
+			        		<br>
+			        		<span class="hide_color">期</span>
+			        		<span class="hide_color">許</span>
+			        		<span class="show_color">自</span>
+			        		<span class="hide_color">己</span>
+			        		<span class="show_color">可</span>
+			        		<span class="hide_color">以</span>
+			        		<span class="hide_color">一</span>
+			        		<span class="show_color">次</span>
+			        		<span class="hide_color">又</span>
+			        		<span class="hide_color">一</span>
+			        		<span class="hide_color">次</span>
+			        		<span class="show_color">地</span>
+			        		<span class="hide_color">感</span>
+			        		<span class="show_color">動</span>
+			        		<span class="hide_color">他</span>
+			        		<span class="hide_color">人</span>
+			        	</div>
+			        </div>
+
 			        <div class="view view4">
-			            <div class="draw">
-			            </div>
+			        view4
 			        </div>
 			        <div class="view view5">view5 </div>
 			        <div class="view view6">view6 </div>
