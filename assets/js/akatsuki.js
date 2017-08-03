05e54df84c2c7fd7e94658fd18dbbd75862777be
FlyC <yayayahahahaooii@gmail.com>
2017-08-03 13:55:16 +0800

    sort out the javascript file.

--- assets/js/akatsuki.js
+++ assets/js/akatsuki.js
@@ -4,6 +4,9 @@ document.addEventListener("DOMContentLoaded", function() {
 		clickMaker(btns[i], i);
 	}
 
+	/* down below is content part, ignore it for temporally */
+	return;
+
 	/* seperate sentence */
 	var sentences = document.querySelectorAll(".content");
 	var spans = [];
@@ -51,19 +54,12 @@ function clickMaker(obj, number, callback) {
 function messArray(array) {
 	var currentIndex = array.length,
 		temporaryValue, randomIndex;
-
-	// While there remain elements to shuffle...
 	while (0 !== currentIndex) {
-
-		// Pick a remaining element...
 		randomIndex = Math.floor(Math.random() * currentIndex);
 		currentIndex -= 1;
-
-		// And swap it with the current element.
 		temporaryValue = array[currentIndex];
 		array[currentIndex] = array[randomIndex];
 		array[randomIndex] = temporaryValue;
 	}
-
 	return array;
 }
