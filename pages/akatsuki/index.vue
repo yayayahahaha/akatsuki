7c911d0a1b4760479f4b1267690d0bcc54e02bf2
FlyC <yayayahahahaooii@gmail.com>
2017-08-03 14:54:43 +0800

    opening works

--- pages/akatsuki/index.vue
+++ pages/akatsuki/index.vue
@@ -49,6 +49,65 @@
 			        </div>
 			        <div class="view view2">
 			            <div class="content">
+			            <span class="show_color">我</span>
+			            <span class="hide_color">是</span>
+			            <span class="hide_color">鍾</span>
+			            <span class="hide_color">維</span>
+			            <span class="show_color">聖</span>
+			            <br>
+			            <span class="hide_color">致</span>
+			            <span class="hide_color">力</span>
+			            <span class="show_color">於</span>
+			            <span class="hide_color">做</span>
+			            <span class="hide_color">出</span>
+			            <span class="hide_color">令</span>
+			            <span class="show_color">人</span>
+			            <span class="hide_color">驚</span>
+			            <span class="hide_color">豔</span>
+			            <span class="hide_color">的</span>
+			            <span class="show_color">網</span>
+			            <span class="hide_color">站</span>
+			            <br>
+			            <br>
+			            <span class="show_color">相</span>
+			            <span class="hide_color">信</span>
+			            <span class="hide_color">人</span>
+			            <span class="hide_color">生</span>
+			            <span class="show_color">是</span>
+			            <span class="hide_color">一</span>
+			            <span class="hide_color">場</span>
+			            <span class="hide_color">驚</span>
+			            <span class="show_color">險</span>
+			            <span class="hide_color">刺</span>
+			            <span class="hide_color">激</span>
+			            <span class="hide_color">的</span>
+			            <span class="show_color">旅</span>
+			            <span class="hide_color">途</span>
+			            <br>
+			            <span class="hide_color">在</span>
+			            <span class="show_color">藝</span>
+			            <span class="hide_color">術</span>
+			            <span class="hide_color">與</span>
+			            <span class="hide_color">邏</span>
+			            <span class="show_color">輯</span>
+			            <span class="hide_color">間</span>
+			            <span class="hide_color">取</span>
+			            <span class="hide_color">得</span>
+			            <span class="show_color">平</span>
+			            <span class="hide_color">衡</span>
+			            <span class="hide_color">的</span>
+			            <span class="hide_color">道</span>
+			            <span class="show_color">路</span>
+			            <span class="hide_color">上</span>
+			            <br>
+			            <span class="hide_color">今</span>
+			            <span class="show_color">後</span>
+			            <span class="hide_color">也</span>
+			            <span class="hide_color">會</span>
+			            <span class="hide_color">繼</span>
+			            <span class="show_color">續</span>
+			            <span class="hide_color">努</span>
+			            <span class="hide_color">力</span>
 			            </div>
 			        </div>
 
@@ -90,7 +149,67 @@
 			        	<span class="show_color">!</span>
 			        </div>
 			        <div class="view view2">
+			            <img src="/images/profile1.jpg" alt="profile">
 			            <div class="content">
+			            <span class="hide_color">我</span>
+			            <span class="show_color">是</span>
+			            <span class="hide_color">鍾</span>
+			            <span class="hide_color">維</span>
+			            <span class="hide_color">聖</span>
+			            <br>
+			            <span class="hide_color">致</span>
+			            <span class="hide_color">力</span>
+			            <span class="hide_color">於</span>
+			            <span class="show_color">做</span>
+			            <span class="hide_color">出</span>
+			            <span class="hide_color">令</span>
+			            <span class="hide_color">人</span>
+			            <span class="show_color">驚</span>
+			            <span class="hide_color">豔</span>
+			            <span class="hide_color">的</span>
+			            <span class="hide_color">網</span>
+			            <span class="show_color">站</span>
+			            <br>
+			            <br>
+			            <span class="hide_color">相</span>
+			            <span class="show_color">信</span>
+			            <span class="hide_color">人</span>
+			            <span class="hide_color">生</span>
+			            <span class="hide_color">是</span>
+			            <span class="show_color">一</span>
+			            <span class="hide_color">場</span>
+			            <span class="hide_color">驚</span>
+			            <span class="hide_color">險</span>
+			            <span class="show_color">刺</span>
+			            <span class="hide_color">激</span>
+			            <span class="hide_color">的</span>
+			            <span class="hide_color">旅</span>
+			            <span class="show_color">途</span>
+			            <br>
+			            <span class="hide_color">在</span>
+			            <span class="hide_color">藝</span>
+			            <span class="show_color">術</span>
+			            <span class="hide_color">與</span>
+			            <span class="hide_color">邏</span>
+			            <span class="hide_color">輯</span>
+			            <span class="show_color">間</span>
+			            <span class="hide_color">取</span>
+			            <span class="hide_color">得</span>
+			            <span class="hide_color">平</span>
+			            <span class="show_color">衡</span>
+			            <span class="hide_color">的</span>
+			            <span class="hide_color">道</span>
+			            <span class="hide_color">路</span>
+			            <span class="show_color">上</span>
+			            <br>
+			            <span class="hide_color">今</span>
+			            <span class="hide_color">後</span>
+			            <span class="show_color">也</span>
+			            <span class="hide_color">會</span>
+			            <span class="hide_color">繼</span>
+			            <span class="hide_color">續</span>
+			            <span class="show_color">努</span>
+			            <span class="hide_color">力</span>
 			            </div>
 			        </div>
 
@@ -133,6 +252,65 @@
 			        </div>
 			        <div class="view view2">
 			            <div class="content">
+			            <span class="hide_color">我</span>
+			            <span class="hide_color">是</span>
+			            <span class="show_color">鍾</span>
+			            <span class="hide_color">維</span>
+			            <span class="hide_color">聖</span>
+			            <br>
+			            <span class="show_color">致</span>
+			            <span class="hide_color">力</span>
+			            <span class="hide_color">於</span>
+			            <span class="hide_color">做</span>
+			            <span class="show_color">出</span>
+			            <span class="hide_color">令</span>
+			            <span class="hide_color">人</span>
+			            <span class="hide_color">驚</span>
+			            <span class="show_color">豔</span>
+			            <span class="hide_color">的</span>
+			            <span class="hide_color">網</span>
+			            <span class="hide_color">站</span>
+			            <br>
+			            <br>
+			            <span class="hide_color">相</span>
+			            <span class="hide_color">信</span>
+			            <span class="show_color">人</span>
+			            <span class="hide_color">生</span>
+			            <span class="hide_color">是</span>
+			            <span class="hide_color">一</span>
+			            <span class="show_color">場</span>
+			            <span class="hide_color">驚</span>
+			            <span class="hide_color">險</span>
+			            <span class="hide_color">刺</span>
+			            <span class="show_color">激</span>
+			            <span class="hide_color">的</span>
+			            <span class="hide_color">旅</span>
+			            <span class="hide_color">途</span>
+			            <br>
+			            <span class="hide_color">在</span>
+			            <span class="hide_color">藝</span>
+			            <span class="hide_color">術</span>
+			            <span class="show_color">與</span>
+			            <span class="hide_color">邏</span>
+			            <span class="hide_color">輯</span>
+			            <span class="hide_color">間</span>
+			            <span class="show_color">取</span>
+			            <span class="hide_color">得</span>
+			            <span class="hide_color">平</span>
+			            <span class="hide_color">衡</span>
+			            <span class="show_color">的</span>
+			            <span class="hide_color">道</span>
+			            <span class="hide_color">路</span>
+			            <span class="hide_color">上</span>
+			            <br>
+			            <span class="hide_color">今</span>
+			            <span class="hide_color">後</span>
+			            <span class="hide_color">也</span>
+			            <span class="show_color">會</span>
+			            <span class="hide_color">繼</span>
+			            <span class="hide_color">續</span>
+			            <span class="hide_color">努</span>
+			            <span class="show_color">力</span>
 			            </div>
 			        </div>
 
@@ -175,6 +353,65 @@
 			        </div>
 			        <div class="view view2">
 			            <div class="content">
+			            <span class="hide_color">我</span>
+			            <span class="hide_color">是</span>
+			            <span class="hide_color">鍾</span>
+			            <span class="show_color">維</span>
+			            <span class="hide_color">聖</span>
+			            <br>
+			            <span class="hide_color">致</span>
+			            <span class="show_color">力</span>
+			            <span class="hide_color">於</span>
+			            <span class="hide_color">做</span>
+			            <span class="hide_color">出</span>
+			            <span class="show_color">令</span>
+			            <span class="hide_color">人</span>
+			            <span class="hide_color">驚</span>
+			            <span class="hide_color">豔</span>
+			            <span class="show_color">的</span>
+			            <span class="hide_color">網</span>
+			            <span class="hide_color">站</span>
+			            <br>
+			            <br>
+			            <span class="hide_color">相</span>
+			            <span class="hide_color">信</span>
+			            <span class="hide_color">人</span>
+			            <span class="show_color">生</span>
+			            <span class="hide_color">是</span>
+			            <span class="hide_color">一</span>
+			            <span class="hide_color">場</span>
+			            <span class="show_color">驚</span>
+			            <span class="hide_color">險</span>
+			            <span class="hide_color">刺</span>
+			            <span class="hide_color">激</span>
+			            <span class="show_color">的</span>
+			            <span class="hide_color">旅</span>
+			            <span class="hide_color">途</span>
+			            <br>
+			            <span class="show_color">在</span>
+			            <span class="hide_color">藝</span>
+			            <span class="hide_color">術</span>
+			            <span class="hide_color">與</span>
+			            <span class="show_color">邏</span>
+			            <span class="hide_color">輯</span>
+			            <span class="hide_color">間</span>
+			            <span class="hide_color">取</span>
+			            <span class="show_color">得</span>
+			            <span class="hide_color">平</span>
+			            <span class="hide_color">衡</span>
+			            <span class="hide_color">的</span>
+			            <span class="show_color">道</span>
+			            <span class="hide_color">路</span>
+			            <span class="hide_color">上</span>
+			            <br>
+			            <span class="show_color">今</span>
+			            <span class="hide_color">後</span>
+			            <span class="hide_color">也</span>
+			            <span class="hide_color">會</span>
+			            <span class="show_color">繼</span>
+			            <span class="hide_color">續</span>
+			            <span class="hide_color">努</span>
+			            <span class="hide_color">力</span>
 			            </div>
 			        </div>
 
