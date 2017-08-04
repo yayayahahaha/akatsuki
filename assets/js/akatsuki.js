e95f512b64ee4d17ab2ba7f014565a57bb7da048
FlyC <yayayahahahaooii@gmail.com>
2017-08-04 15:33:33 +0800

    remove useless code

--- assets/js/akatsuki.js
+++ assets/js/akatsuki.js
@@ -6,50 +6,7 @@ document.addEventListener("DOMContentLoaded", function() {
 
 	/* down below is content part, ignore it for temporally */
 	return;
-
-	/* seperate sentence */
-	var sentences = document.querySelectorAll(".content");
-	var spans = [];
-
-	/* make a shuffle number array */
-	var numbers = [];
-	for (var i = 0; i < sentences[0].innerHTML.length; i++) {
-		numbers.push(i);
-	}
-	numbers = messArray(numbers);
-
-	/* make four array has many span with word innerHTML */
-	for (var i = 0; i < sentences.length; i++) {
-
-		spans[i] = [];
-		for (var j = 0; j < sentences[i].innerHTML.length; j++) {
-			var span = document.createElement("span");
-			span.innerHTML = sentences[i].innerHTML[j];
-			spans[i].push(span);
-		}
-
-		/* clean up each sentence and append span */
-		sentences[i].innerHTML = "";
-		for (var j = 0; j < spans[i].length; j++) {
-			sentences[i].appendChild(spans[i][j]);
-		}
-
-		/* use shuffle number array decide which span should change color */
-		words = sentences[i].childNodes;
-		for (var j = i * Math.floor(words.length / 4); j < (i + 1) * Math.floor(words.length / 4); j++) {
-			words[numbers[j]].classList.add('show_color');
-		}
-	}
 });
-
-function clickMaker(obj, number, callback) {
-	obj.onclick = function() {
-		$(".perspectiveView").animate({
-				scrollTop: document.all.forHeight.offsetHeight * number,
-			},
-			1500);
-	}
-}
 
 function messArray(array) {
 	var currentIndex = array.length,
