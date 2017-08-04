cc010156dfef9185bce478fa37da013a841832b9
FlyC <yayayahahahaooii@gmail.com>
2017-08-04 13:29:46 +0800

    fix render problem of jump of charactor <>

--- pages/akatsuki/index.vue
+++ pages/akatsuki/index.vue
@@ -21,95 +21,9 @@
 		</div>
 		<div class="perspectiveView">
 			<div class="layer layer1">
-				<div class="view view1">
-					<span class="show_color">s</span>
-					<span class="hide_color">c</span>
-					<span class="hide_color">r</span>
-					<span class="hide_color">o</span>
-					<span class="show_color">l</span>
-					<span class="hide_color">l</span>
-					<span class="hide_color">_</span>
-					<span class="hide_color">d</span>
-					<span class="show_color">o</span>
-					<span class="hide_color">w</span>
-					<span class="hide_color">n</span>
-					<span class="hide_color">_</span>
-					<span class="hide_color">a</span>
-					<span class="show_color">n</span>
-					<span class="hide_color">d</span>
-					<span class="hide_color">_</span>
-					<span class="hide_color">h</span>
-					<span class="hide_color">a</span>
-					<span class="show_color">v</span>
-					<span class="hide_color">e</span>
-					<span class="hide_color">_</span>
-					<span class="hide_color">f</span>
-					<span class="hide_color">u</span>
-					<span class="show_color">n</span>
-					<span class="hide_color">!</span>
-				</div>
+				<op1></op1>
 				<div class="view view2">
-					<div class="content">
-						<span class="show_color">我</span>
-						<span class="hide_color">是</span>
-						<span class="hide_color name_style">鍾</span>
-						<span class="hide_color name_style">維</span>
-						<span class="show_color name_style">聖</span>
-						<br>
-						<span class="hide_color">致</span>
-						<span class="hide_color">力</span>
-						<span class="show_color">於</span>
-						<span class="hide_color">做</span>
-						<span class="hide_color">出</span>
-						<span class="hide_color">令</span>
-						<span class="show_color">人</span>
-						<span class="hide_color">驚</span>
-						<span class="hide_color">豔</span>
-						<span class="hide_color">的</span>
-						<span class="show_color">網</span>
-						<span class="hide_color">站</span>
-						<br>
-						<br>
-						<span class="show_color">相</span>
-						<span class="hide_color">信</span>
-						<span class="hide_color">人</span>
-						<span class="hide_color">生</span>
-						<span class="show_color">是</span>
-						<span class="hide_color">一</span>
-						<span class="hide_color">場</span>
-						<span class="hide_color">驚</span>
-						<span class="show_color">險</span>
-						<span class="hide_color">刺</span>
-						<span class="hide_color">激</span>
-						<span class="hide_color">的</span>
-						<span class="show_color">旅</span>
-						<span class="hide_color">途</span>
-						<br>
-						<span class="hide_color">在</span>
-						<span class="show_color">藝</span>
-						<span class="hide_color">術</span>
-						<span class="hide_color">與</span>
-						<span class="hide_color">邏</span>
-						<span class="show_color">輯</span>
-						<span class="hide_color">間</span>
-						<span class="hide_color">取</span>
-						<span class="hide_color">得</span>
-						<span class="show_color">平</span>
-						<span class="hide_color">衡</span>
-						<span class="hide_color">的</span>
-						<span class="hide_color">道</span>
-						<span class="show_color">路</span>
-						<span class="hide_color">上</span>
-						<br>
-						<span class="hide_color">今</span>
-						<span class="show_color">後</span>
-						<span class="hide_color">也</span>
-						<span class="hide_color">會</span>
-						<span class="hide_color">繼</span>
-						<span class="show_color">續</span>
-						<span class="hide_color">努</span>
-						<span class="hide_color">力</span>
-					</div>
+					<intro2></intro2>
 				</div>
 				<div class="view view3">
 					<img src="/images/profile2.jpg" alt="profile">
@@ -143,96 +57,10 @@
 				<div class="view view9">view9 </div>
 			</div>
 			<div class="layer layer2">
-				<div class="view view1">                    
-					<span class="hide_color">s</span>
-					<span class="show_color">c</span>
-					<span class="hide_color">r</span>
-					<span class="hide_color">o</span>
-					<span class="hide_color">l</span>
-					<span class="show_color">l</span>
-					<span class="hide_color">_</span>
-					<span class="hide_color">d</span>
-					<span class="hide_color">o</span>
-					<span class="show_color">w</span>
-					<span class="hide_color">n</span>
-					<span class="hide_color">_</span>
-					<span class="hide_color">a</span>
-					<span class="hide_color">n</span>
-					<span class="show_color">d</span>
-					<span class="hide_color">_</span>
-					<span class="hide_color">h</span>
-					<span class="hide_color">a</span>
-					<span class="hide_color">v</span>
-					<span class="show_color">e</span>
-					<span class="hide_color">_</span>
-					<span class="hide_color">f</span>
-					<span class="hide_color">u</span>
-					<span class="hide_color">n</span>
-					<span class="show_color">!</span>
-				</div>
+				<op2></op2>
 				<div class="view view2">
 					<img src="/images/profile1.jpg" alt="profile">
-					<div class="content">
-						<span class="hide_color">我</span>
-						<span class="show_color">是</span>
-						<span class="hide_color name_style">鍾</span>
-						<span class="hide_color name_style">維</span>
-						<span class="hide_color name_style">聖</span>
-						<br>
-						<span class="hide_color">致</span>
-						<span class="hide_color">力</span>
-						<span class="hide_color">於</span>
-						<span class="show_color">做</span>
-						<span class="hide_color">出</span>
-						<span class="hide_color">令</span>
-						<span class="hide_color">人</span>
-						<span class="show_color">驚</span>
-						<span class="hide_color">豔</span>
-						<span class="hide_color">的</span>
-						<span class="hide_color">網</span>
-						<span class="show_color">站</span>
-						<br>
-						<br>
-						<span class="hide_color">相</span>
-						<span class="show_color">信</span>
-						<span class="hide_color">人</span>
-						<span class="hide_color">生</span>
-						<span class="hide_color">是</span>
-						<span class="show_color">一</span>
-						<span class="hide_color">場</span>
-						<span class="hide_color">驚</span>
-						<span class="hide_color">險</span>
-						<span class="show_color">刺</span>
-						<span class="hide_color">激</span>
-						<span class="hide_color">的</span>
-						<span class="hide_color">旅</span>
-						<span class="show_color">途</span>
-						<br>
-						<span class="hide_color">在</span>
-						<span class="hide_color">藝</span>
-						<span class="show_color">術</span>
-						<span class="hide_color">與</span>
-						<span class="hide_color">邏</span>
-						<span class="hide_color">輯</span>
-						<span class="show_color">間</span>
-						<span class="hide_color">取</span>
-						<span class="hide_color">得</span>
-						<span class="hide_color">平</span>
-						<span class="show_color">衡</span>
-						<span class="hide_color">的</span>
-						<span class="hide_color">道</span>
-						<span class="hide_color">路</span>
-						<span class="show_color">上</span>
-						<br>
-						<span class="hide_color">今</span>
-						<span class="hide_color">後</span>
-						<span class="show_color">也</span>
-						<span class="hide_color">會</span>
-						<span class="hide_color">繼</span>
-						<span class="hide_color">續</span>
-						<span class="show_color">努</span>
-						<span class="hide_color">力</span>
-					</div>
+					<intro2></intro2>
 				</div>
 				<div class="view view3">
 					<img src="/images/timeline.png" alt="timeline">
@@ -255,7 +83,7 @@
 						<section>
 							<h2>SVG</h2>
 							<div class="hide_n_show">
-								<p>標籤 &#060;path&#062; &#060;def&#062; 使用 </p>
+								<p>標籤 path, def 使用 </p>
 								<p>視域Viewport 概念</p>
 							</div>
 						</section>
